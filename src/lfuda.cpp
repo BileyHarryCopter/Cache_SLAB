f97d669c450185ab5ed707a55006ae177d17456b
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-09-18 00:37:03 +0300

    Working lfuda which needs in tests. Also creating slow version of lfuda

--- src/lfuda.cpp
+++ src/lfuda.cpp
@@ -5,24 +5,24 @@
 // slow get page imitation
 int slow_get_page(int key) { return key; }
 
-int contest_processing (size_t cache_cap, int requests)
+std::size_t contest_processing (size_t cache_cap, int requests)
 {
     chc::lfuda_t<int> cache {cache_cap};
 
     int ireq = 0;
-    int hits = 0;
+    std::size_t hits = 0;
 
     for (int i = 0; i < requests; i++)
     {
         std::cin >> ireq;
         assert (std::cin.good());
         
-        cache.cache_dump(ireq);
+        //  cache.cache_dump(ireq);
 
         if (cache.lookup_update (ireq, slow_get_page))
             hits++;
     }
-    cache.cache_dump(ireq);
+    //  cache.cache_dump(ireq);
 
     return hits;
 }
@@ -35,7 +35,7 @@ int main (void)
     std::cin >> init_cap >> reqs;
     assert (std::cin.good());
 
-    int hits = contest_processing (init_cap, reqs);
+    std::size_t hits = contest_processing (init_cap, reqs);
     std::cout << "Hits: " << hits << std::endl;
 
     return 0;
--- src/lfuda.hpp
+++ src/lfuda.hpp
@@ -11,11 +11,13 @@
 namespace chc
 {
     //  cache_t class: cache_cap, hashmap, list
+enum {MAX_AGE = 100000};
+
 template <typename page_t, typename key_t = int> class lfuda_t 
 {
-    size_t capacity;        //  capacity of cache
-    size_t size = 0;        //  number of elements in cache
-    size_t age = 0;         //  characteristic for realisation of LFUDA
+    std::size_t capacity;        //  capacity of cache
+    std::size_t size = 0;        //  number of elements in cache
+    std::size_t age = 0;         //  characteristic for realisation of LFUDA
 
     //  wlist_t --- list of node with weight
     //  wlnode_t --- weight node contains pointer to appropriate local list 
@@ -36,19 +38,19 @@ template <typename page_t, typename key_t = int> class lfuda_t
 
     struct wlnode_t
     {
-        size_t weight;
+        std::size_t weight;
         std::list<llnode_t> local_list;                              //  nodes of local list with page
     };
 
     std::list<wlnode_t> wllist;                                      //   nodes of weight list with local list as node  
     std::unordered_map<key_t, llnode_it> hashmap;                       //   unordered_map
-    std::map<key_t, wlnode_it> RBtree;             //   rbtree
+    std::map<std::size_t, wlnode_it> RBtree;             //   rbtree
 
     //  do I really nead iterator to hashmap and rbtree???
     //  using hashmap_it = typename std::unordered_map<page_t, key_t>::iterator;
 
 public:
-    lfuda_t (size_t capacity_) : capacity{capacity_} {};
+    lfuda_t (std::size_t capacity_) : capacity{capacity_} {};
 
     bool is_full () const { return capacity == size; }
 
@@ -94,17 +96,28 @@ public:
     }
 
 private:
+    void is_age_reset () 
+    {
+        if (age > MAX_AGE)
+            age = 0;
+    }
     template <typename func> void push_cache (const key_t& key, const func slow_get_page) 
     {
         if (is_full())      //  this full about size of cache - not about size of hashmap
         {
-            auto evict_node = std::prev(wllist.begin()->local_list.end());      //  access to iterator to last node in the local list
-            hashmap.erase(evict_node->key);
-            age = wllist.begin()->weight;
-            wllist.begin()->local_list.erase(evict_node);
+            auto evict_node = std::prev(wllist.begin()->local_list.end());      //  access to iterator to the last node in the local list
+            hashmap.erase(evict_node->key);  
 
+            age = wllist.begin()->weight;
+
+            wllist.begin()->local_list.erase(evict_node);
             if (wllist.begin()->local_list.empty())
+            {
+                RBtree.erase (wllist.begin()->weight);
                 wllist.pop_front ();
+            }
+
+            //  What about arese in the RBtree, you're monster?!?!??!
 
             size--;
         }
@@ -117,17 +130,25 @@ private:
 
     void cache_update (const key_t& key)
     {
-        //  increase weight found llnode and moving it into cache
+        //  increase weight found llnode and move it into cache
         auto need_llnode = hashmap.find(key)->second;
+        auto old_wlnode = need_llnode->parent;
 
-        auto need_wlnode = find_need_wlnode (need_llnode->parent->weight + age + 1);
-        need_wlnode->local_list.push_front({key, need_llnode->page, need_llnode->parent});
-
+        auto need_wlnode = find_need_wlnode (old_wlnode->weight + age + 1);
+        need_wlnode->local_list.push_front({key, need_llnode->page, need_wlnode});
         hashmap[key] = need_wlnode->local_list.begin();
-        need_llnode->parent->local_list.erase(need_llnode);
+
+        //  changing a state of RBtree 
+        RBtree.erase (old_wlnode->weight);
+        RBtree [old_wlnode->weight + age + 1] = need_wlnode;
+
+        old_wlnode->local_list.erase(need_llnode);
+        if (old_wlnode->local_list.empty())
+            wllist.erase(old_wlnode);
+
     }
 
-    wlnode_it find_need_wlnode (const int weight)
+    wlnode_it find_need_wlnode (const std::size_t weight)
     {
         auto is_need_wlnode = RBtree.find(weight);
         if (is_need_wlnode == RBtree.end())
@@ -143,10 +164,11 @@ private:
             }
             else
             {
-                more_need_wlnode = std::prev(more_need_wlnode);
-                need_wlnode = more_need_wlnode->second;
+                //  insert need_wlnode before wlnode with upper_weight 
+                need_wlnode = wllist.emplace (more_need_wlnode->second);
+                need_wlnode->weight = weight;
             }
-            
+
             RBtree.insert({weight, need_wlnode});
 
             return need_wlnode;
